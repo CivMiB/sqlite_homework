f793cae8df1f8c9c021564ed3bf0593d9d705142
CivMiB <mibik14@gmail.cpm>
2024-01-31 16:43:55 +0300

    finish

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-'''Создать графическую программу, с использованием Tkinter, которая с выводит список имен, фамилий и их возвраст
-из базы данных sqlite. При запуске программы, должен создаваться (если ее нет) файл базы данных people.db
-и таблица соответственно. База данных должна заполняться в первый раз из словаря. Реализовать все проверки
-на исключения и максимально документировать весь код.'''
-
 from tkinter import ttk
 import tkinter as tk
 import sqlite3
+import os
 
 
 # Создаём класс основного окна
@@ -14,33 +10,76 @@ class App(tk.Tk):
         super().__init__()
         # Устанавливаем параметры окна
         self.title('File Commander')
-        self.geometry('1000x630+50+50')
+        self.geometry('480x300+50+50')
         self.resizable(0, 0)
         self.put_frames()
 
     # Создаём фреймы основного окна
-    # def put_frames(self):
-        # self.add_left_panel = LeftPanel(self).place(x=0, y=0, width=500, height=570)
+    def put_frames(self):
+        self.add_left_panel = Table(self).place(x=0, y=0, width=480, height=300)
+
+class Table(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        # определяем столбцы
+        self.columns = ('id', 'firstname', 'lastname', 'age')
+
+        self.tree = ttk.Treeview(columns=self.columns, show="headings")
+        self.tree.pack(fill='both', expand=1)
+
+        # определяем заголовки
+        self.tree.heading("id", text="Номер п.п.")
+        self.tree.heading("firstname", text="Имя")
+        self.tree.heading("lastname", text="Фамилия")
+        self.tree.heading("age", text="Возраст")
+
+        # настраиваем столбцы
+        self.tree.column("#1", stretch=tk.NO, anchor='c', width=80)
+        self.tree.column("#2", stretch=tk.NO, anchor='c', width=150)
+        self.tree.column("#3", stretch=tk.NO, anchor='c', width=150)
+        self.tree.column("#4", stretch=tk.NO, anchor='c', width=100)
+
+        # Созданём соединение с базой данных
+        with sqlite3.connect('people.db') as conn:
+            # Подключаем курсор для выполнения запросов
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM mytable')
+            for row in cursor.fetchall():
+                self.tree.insert("", tk.END, values=row)
 
 
 def load_db():
-    # Создание базы данных
-    conn = sqlite3.connect('people.db')
-    # Создание таблицы
-    cursor = conn.cursor()
-    # Создание таблицы c проверкой существования таблицы
-    cursor.execute('''CREATE TABLE IF NOT EXISTS mytable (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                   name TEXT,
-                   surname TEXT,
-                   age INTEGER);''')
-    conn.commit()
-    conn.close()
+    # Задаём имя файла
+    db_filename = 'people.db'
+    # Создаём булевую переменную с проверкой на отсутствие файла db_filename
+    db_is_new = not os.path.exists(db_filename)
+    # Созданём соединение с базой данных
+    with sqlite3.connect('people.db') as conn:
+        # Подключаем курсор для выполнения запросов
+        cursor = conn.cursor()
+        # Если файл отсутствует, то:
+        if db_is_new:
+            # Создаём таблицу mytable в БД
+            cursor.execute('''CREATE TABLE mytable (
+                           id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                           firstname TEXT,
+                           lastname TEXT,
+                           age INTEGER);''')
+            # Передаём данные из словаря в таблицу
+            cursor.executemany('INSERT INTO mytable (firstname, lastname, age) VALUES (?,?,?)',
+                               dict_for_start.values())
 
-    
+
 
 if __name__ == "__main__":
+    # Создаём словарь со стартовыми данными
+    dict_for_start = {
+        '1': ('Иван', 'Казаков', 40),
+        '2': ('Владимир', 'Яковлев', 30),
+        '3': ('Александр', 'Александров', 34)
+    }
+    # Запускаем функцию загрузки БД
     load_db()
-    # app = App()
-    # app.mainloop()
+    app = App()
+    app.mainloop()
 
